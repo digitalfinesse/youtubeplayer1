087c7ce5a820cd5bfa4351b658ac7cc5003c17be
digitalfinesse <dizverst@gmail.com>
2017-10-06 15:59:57 -0700

    Add Google Youtube API key

--- app/src/main/java/tech/evgeniypalguev/youtubeplayer/YoutubeActivity.java
+++ app/src/main/java/tech/evgeniypalguev/youtubeplayer/YoutubeActivity.java
@@ -1,19 +1,39 @@
 package tech.evgeniypalguev.youtubeplayer;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends YouTubeBaseActivity
              implements YouTubePlayer.OnInitializedListener{
+    static final String GOOGLE_API_KEY = "AIzaSyC2xQIk1skMiFZyI2tvgX9JMGbYjgkzAKY";
+    static final String YOUTUBE_VIDEO_ID = "B0Fr8cWWD_w";
+    static final String YOUTUBE_PLAYLIST = "PLrCZzMib1e9qLzDXvYnpnJdUsGr3t7fSu";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_youtube);
+//        setContentView(R.layout.activity_youtube);
+//        ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.activity_youtube);
+        ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
+        setContentView(layout);
+
+//        Button button1 = new Button(this);
+//        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300, 80));
+//        button1.setText(("Button added"));
+//        layout.addView(button1);
+
+        YouTubePlayerView player = new YouTubePlayerView(this);
+        player.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                                                                 ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.addView(player);
     }
 
     @Override
